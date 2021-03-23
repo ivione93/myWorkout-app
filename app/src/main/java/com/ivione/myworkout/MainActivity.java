47f4a4879c23a565018bc02b6cb77d3e32d68d7c
Iván García <ivan.garcia.ext@diagroup.com>
2021-03-23 23:16:57 +0100

    Init recyclerView

--- app/src/main/java/com/ivione/myworkout/MainActivity.java
+++ app/src/main/java/com/ivione/myworkout/MainActivity.java
@@ -5,14 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Athlete> listAthletes;
+    RecyclerView rvAthletes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        listAthletes = new ArrayList<>();
+        rvAthletes = findViewById(R.id.recycler_view);
+        rvAthletes.setLayoutManager(new LinearLayoutManager(this));
+
+        addListAthletes();
+
+        AdapterAthlete adapter = new AdapterAthlete(listAthletes);
+        rvAthletes.setAdapter(adapter);
+    }
+
+    private void addListAthletes() {
+        listAthletes.add(new Athlete("M1599","Iván", "García Gómez", "18/06/1993"));
     }
 
     public void addAthlete(View view) {
