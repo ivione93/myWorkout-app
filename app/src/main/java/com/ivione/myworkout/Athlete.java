7d0d294af3d6da7e0b184ff2d04c31b7251f5132
Iván García <ivan.garcia.ext@diagroup.com>
2021-03-24 22:51:54 +0100

    Se introduce bbdd Room

--- app/src/main/java/com/ivione/myworkout/Athlete.java
+++ app/src/main/java/com/ivione/myworkout/Athlete.java
@@ -1,10 +1,21 @@
 package com.ivione.myworkout;
 
-public class Athlete {
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity
+public class Athlete {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "license")
     public String license;
+    @ColumnInfo(name = "name")
     public String name;
+    @ColumnInfo(name = "surname")
     public String surname;
+    @ColumnInfo(name = "birthdate")
     public String birthdate;
 
     public Athlete(String license, String name, String surname, String birthdate) {
