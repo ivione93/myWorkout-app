333c50165cd695df734399367c828732c50178ab
Iván García <ivan.garcia.ext@diagroup.com>
2021-03-24 23:33:00 +0100

    Se añade el boton de eliminar

--- app/src/main/java/com/ivione/myworkout/MainActivity.java
+++ app/src/main/java/com/ivione/myworkout/MainActivity.java
@@ -3,6 +3,7 @@ package com.ivione.myworkout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -41,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
     public void addAthlete(View view) {
         Intent intent = new Intent(this, NewAthlete.class);
         startActivity(intent);
+    }
+
+    public void deleteAthlete(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Se eliminará el atleta", Toast.LENGTH_SHORT);
+        toast.show();
     }
 
 }
