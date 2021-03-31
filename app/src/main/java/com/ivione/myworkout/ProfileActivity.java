3c96084aad5030fd423036c102e38053a39d0dde
Iván García <ivan.garcia.ext@diagroup.com>
2021-03-31 22:23:02 +0200

    BottomNavigationView

--- app/src/main/java/com/ivione/myworkout/ProfileActivity.java
+++ app/src/main/java/com/ivione/myworkout/ProfileActivity.java
@@ -3,10 +3,12 @@ package com.ivione.myworkout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
@@ -15,6 +17,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ivione.myworkout.ui.login.LoginActivity;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -24,7 +27,7 @@ public class ProfileActivity extends AppCompatActivity {
     CircleImageView photoProfile;
     TextView nameProfile, emailProfile, birthProfile, licenseProfile;
     ImageButton btnSignOut;
-    Button btnGoToCompetitions, getBtnGoToTrainings;
+    BottomNavigationView navigation_menu;
 
     GoogleSignInClient mGoogleSignInClient;
     GoogleSignInOptions gso;
@@ -48,20 +51,23 @@ public class ProfileActivity extends AppCompatActivity {
         birthProfile = findViewById(R.id.birthProfile);
         licenseProfile = findViewById(R.id.licenseProfile);
         btnSignOut = findViewById(R.id.btnSignOut);
-        btnGoToCompetitions = findViewById(R.id.btnGoToCompetitions);
-        getBtnGoToTrainings = findViewById(R.id.btnGoToTrainings);
+        navigation_menu = findViewById(R.id.navigation_menu);
 
         btnSignOut.setOnClickListener(v -> signOut());
 
-        btnGoToCompetitions.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ListCompetitions.class);
-            intent.putExtra("license", license);
-            startActivity(intent);
-        });
-
-        getBtnGoToTrainings.setOnClickListener(v -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+        navigation_menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.action_training) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.action_competitions) {
+                    Intent intent = new Intent(getApplicationContext(), ListCompetitions.class);
+                    intent.putExtra("license", license);
+                    startActivity(intent);
+                }
+                return true;
+            }
         });
     }
 
@@ -96,10 +102,10 @@ public class ProfileActivity extends AppCompatActivity {
 
     private void signOut() {
         mGoogleSignInClient.signOut()
-                .addOnCompleteListener(this, task -> {
-                    Intent mainIntent = new Intent(getApplicationContext(), LoginActivity.class);
-                    startActivity(mainIntent);
-                });
+            .addOnCompleteListener(this, task -> {
+                Intent mainIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(mainIntent);
+            });
     }
 
 }
